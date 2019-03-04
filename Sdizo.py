445f5a7598895ce069b3c34282638e77507cf31e
kkulis <krzysiek.kulig@gmail.com>
2019-03-04 13:51:38 +0100

    appending to list

--- Sdizo.py
+++ Sdizo.py
@@ -2,8 +2,12 @@ import random
 #creating txt file
 f=open("baza.txt","w+")
 
-#adding numbers to file
+#adding numbers to file and into list
+base_array=[]
 
 for i in range(10):
     x=random.randint(1,101)
+    base_array.append(x)
     f.write("%d\r\n" % x)
+
+print(base_array)
