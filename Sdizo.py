4aa365e5c7144e8c796f723c16ef0cca4972721a
kkulis <krzysiek.kulig@gmail.com>
2019-03-04 14:53:02 +0100

    more numbers

--- Sdizo.py
+++ Sdizo.py
@@ -47,8 +47,8 @@ f=open("baza.txt","w+")
 #adding numbers to file and into list
 base_array=[]
 
-for i in range(10):
-    x=random.randint(1,101)
+for i in range(100):
+    x=random.randint(1,1001)
     base_array.append(x)
     f.write("%d\r\n" % x)
 
